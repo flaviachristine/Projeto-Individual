0715a6e7247ea74b923510e698c457501d5c6d3c
flaviachristine <flavia.pereira@sptech.school>
2025-05-31 17:51:10 -0300

    adicionando nome a home page

--- public/JS/sessao.js
+++ public/JS/sessao.js
@@ -2,36 +2,36 @@
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
-    var senha = sessionStorage.SENHA_USUARIO;
+    // var senha = sessionStorage.SENHA_USUARIO;
 
     var b_usuario = document.getElementById("b_usuario");
 
-    if (email != null && nome != null && senha != null) {
+    if (email != null && nome != null ) {
         b_usuario.innerHTML = nome;
     } else {
         window.location = "../home.html";
     }
 }
 
-function limparSessao() {
-    sessionStorage.clear();
-    window.location = "../login.html";
-}
+// function limparSessao() {
+//     sessionStorage.clear();
+//     window.location = "../login.html";
+// }
 
 // carregamento (loading)
-function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
-}
+// function aguardar() {
+//     var divAguardar = document.getElementById("div_aguardar");
+//     divAguardar.style.display = "flex";
+// }
 
-function finalizarAguardar(texto) {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
+// function finalizarAguardar(texto) {
+//     var divAguardar = document.getElementById("div_aguardar");
+//     divAguardar.style.display = "none";
 
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.style.display = "flex";
-        divErrosLogin.innerHTML = texto;
-    }
-}
+//     var divErrosLogin = document.getElementById("div_erros_login");
+//     if (texto) {
+//         divErrosLogin.style.display = "flex";
+//         divErrosLogin.innerHTML = texto;
+//     }
+// }
 
