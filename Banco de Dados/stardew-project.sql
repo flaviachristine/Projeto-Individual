446ce0ffe1e5ecd750adec0c8d2201134b5e4239
flaviachristine <flavia.pereira@sptech.school>
2025-06-10 00:45:34 -0300

    banco de dados atualizado para nova regra de negócio

--- Banco de Dados/stardew-project.sql
+++ Banco de Dados/stardew-project.sql
@@ -6,13 +6,14 @@ create table usuario(
 	idUsuario int primary key auto_increment,
 	nome VARCHAR(45) not null,
 	email VARCHAR(45) not null unique,
-	senha VARCHAR(45) not null
-);
+	senha VARCHAR(45) not null,
+    carteira int not null
+); 
 
 create table quiz(
 	idQuiz int primary key auto_increment,
-    qtdQuestoes int,
-    tema varchar(45)
+    qtdQuestoes int not null,
+    tema varchar(45) not null
 );
 insert into quiz values
 (1, 5, 'Personalidade');
@@ -29,19 +30,34 @@ create table tentativa(
 			foreign key (fkQuiz) 
             references quiz(idQuiz),
 	dtTentativa datetime default current_timestamp,
-	npc varchar(20)
+	npc varchar(20) not null
 );
 
 create table venda(
 	idVenda int primary key auto_increment,
-	fkUsuario int,
+	fkUsuario int not null,
 	constraint fkUsuarioVenda foreign key (fkUsuario) references usuario(idUsuario),
-	qtdVendida int,
-	itemVendido varchar(50),
-	valorVenda int,
+	qtdVendida int not null,
+	itemVendido varchar(50) not null,
+	valorVenda int not null,
 	dtVenda datetime default current_timestamp
 );
+create table compra(
+	idVenda int primary key auto_increment,
+	fkUsuario int not null,
+	constraint fkUsuarioCompra foreign key (fkUsuario) references usuario(idUsuario),
+	qtdComprada int not null,
+	itemComprado varchar(50) not null,
+	valorCompra int not null,
+	dtCompra datetime default current_timestamp
+);
 
-select * from usuario;
-select * from tentativa;
+create table historicoCarteira(
+	idHistorico int primary key auto_increment,
+	valorCarteiraAtual int not null,
+	fkUsuario int,
+	constraint fkUsuarioHistorico foreign key (fkUsuario) references usuario(idUsuario),
+    dataCarteira datetime default current_timestamp
+);
+
 
